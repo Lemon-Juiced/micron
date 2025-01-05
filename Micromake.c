28e8c212064da2ad0d2a8c6b38b5d1bde202d700
Lemon-Juiced <47193272+Lemon-Juiced@users.noreply.github.com>
2025-01-04 21:56:02 -0500

    Correct file passing

--- Micromake.c
+++ Micromake.c
@@ -16,22 +16,27 @@
 int main(int argc, char *argv[]) {
     // Check if the user has provided a Micron program to run
     if (argc < 2) {
-        printf("Usage: micron <program>\n");
+        printf("Micromake Usage: micron <program>\n");
         return 1;
     }
+
+    // Get the absolute path of the file to run
+    char abs_path[300];
+    _fullpath(abs_path, argv[argc - 1], sizeof(abs_path));
 
     // Check if the user provided the -r flag
     if (argc == 3 && strcmp(argv[1], "-r") == 0) {
         // Run the Micron program from the /target/release/ folder
-        char command[100] = "cd target/release && ./";
+        char command[400];
         system("cargo build --release"); // Build the Micron program in release mode
-        strcat(command, argv[2]);
+        snprintf(command, sizeof(command), "cd target/release && micron.exe \"%s\"", abs_path);
         system(command);
     } else {
         // Run the Micron program from the /target/debug/ folder
-        char command[100] = "cd target/debug && ./";
+        char command[400];
         system("cargo build"); // Build the Micron program in debug mode
-        strcat(command, argv[1]);
+        snprintf(command, sizeof(command), "cd target/debug && micron.exe \"%s\"", abs_path);
         system(command);
     }
+    return 0;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
-// The Micron Interpreter Itself
+use std::env;
+use std::fs;
 
+// The Micron Interpreter Itself
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Interpreter Usage: micron <file>");
+        std::process::exit(1);
+    }
+
+    let filename = &args[1];
+    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+
+    println!("File contents:\n{}", contents);
 }
