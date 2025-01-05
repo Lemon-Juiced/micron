781463231b20ad915281c596d2017ae6e78312b0
Lemon-Juiced <47193272+Lemon-Juiced@users.noreply.github.com>
2025-01-05 00:09:16 -0500

    Added check to make sure program is Micron file

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use std::fs;
 
 /// The main function of the Micron interpreter.
 fn main() {
+    // Get the command line arguments
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
         // This should be theoretically when called from Micromake, but this can be called without Micromake outside of the development environment
@@ -14,8 +15,16 @@ fn main() {
         print_warning(""); // Temporary fix for to prevent the warning message in terminal
     }
 
+    // Get the filename
     let filename = &args[1];
-    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
 
+    // Get the file extension, if file extension is not .mcrn, print an error message and exit the program
+    let file_extension = filename.split('.').last().unwrap();
+    if file_extension != "mcrn" {
+        print_error("File extension must be .mcrn");
+    }
+
+    // Test: Print file contents
+    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
     println!("File contents:\n{}", contents);
 }
